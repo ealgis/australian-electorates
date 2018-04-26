3dedd8b777b7a5a679beba5307956144d870538a
Grahame Bowland <grahame@angrygoats.net>
2018-04-26 21:00:30 +0800

    update to latest ealgis-common

--- recipe.py
+++ recipe.py
@@ -93,7 +93,7 @@ def load_shapes(factory, basedir, tmpdir):
 def main():
     tmpdir = "/app/tmp"
     basedir = '/app/'
-    factory = DataLoaderFactory("scratch", clean=False)
+    factory = DataLoaderFactory(db_name="scratch", clean=False)
     shape_results = load_shapes(factory, basedir, tmpdir)
     for result in shape_results:
         result.dump(tmpdir)
